19b4aea59c4ba79ab52254fe8b3d6f82821c4328
sagartalukdar <121626196+sagartalukdar@users.noreply.github.com>
2024-12-21 21:14:32 +0530

    Update Api.ts

--- src/Redux/Config/Api.ts
+++ src/Redux/Config/Api.ts
@@ -1,6 +1,7 @@
 import axios from "axios"
 
-export const API_URL="http://localhost:8080"
+// export const API_URL="http://localhost:8080"
+export const API_URL="https://ecommerce-multivendor.up.railway.app"
 
 export const api=axios.create({
     baseURL:API_URL,
