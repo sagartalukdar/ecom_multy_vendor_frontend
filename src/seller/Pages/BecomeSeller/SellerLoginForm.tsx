cf407b2f17bad40c5e19c8e297993d374667c1ac
sagartalukdar <121626196+sagartalukdar@users.noreply.github.com>
2024-12-23 20:04:00 +0530

    Update SellerLoginForm.tsx

--- src/seller/Pages/BecomeSeller/SellerLoginForm.tsx
+++ src/seller/Pages/BecomeSeller/SellerLoginForm.tsx
@@ -23,42 +23,46 @@ const SellerLoginForm = () => {
      dispatch(sendLoginSignupOtp({email:formik.values.email}))
   }  
 
-  return (
+   return (
     <Box>
     <div className='space-y-5'>
       <h1 className="text-center font-bold text-xl text-primary-color">Login as seller</h1>
-      <TextField
-          fullWidth
-          name='email'
-          label="Email"
-          value={formik.values.email}
-          onChange={formik.handleChange}
-          error={formik.touched.email && Boolean(formik.errors.email)}
-          helperText={formik.touched.email && formik.errors.email}
-          onBlur={formik.handleBlur}
-      />
-      {true &&
-        <div className="space-y-2">
-          <p className="font-medium text-sm opacity-60">enter OTP sent to your email</p>
-       <TextField
-          fullWidth
-          name='otp'
-          label="OTP"
-          value={formik.values.otp}
-          onChange={formik.handleChange}
-          error={formik.touched.otp && Boolean(formik.errors.otp)}
-          helperText={formik.touched.otp && formik.errors.otp}
-          onBlur={formik.handleBlur}
-      />
-      </div>
-      }
-  
-      <Button onClick={handleSendOtp} fullWidth sx={{py:"11px"}} variant='contained'>
-        sent otp
-      </Button>
-      <Button onClick={()=>formik.handleSubmit()} fullWidth sx={{py:"11px"}} variant='contained'>
-         login
-      </Button>
+
+       {!auth.otp &&
+          <div className="space-y-3">
+            <TextField
+                fullWidth
+                name='email'
+                label="Email"
+                value={formik.values.email}
+                onChange={formik.handleChange}
+                error={formik.touched.email && Boolean(formik.errors.email)}
+                helperText={formik.touched.email && formik.errors.email}
+                onBlur={formik.handleBlur}
+            />
+            <Button disabled={auth.loading} onClick={handleSendOtp} fullWidth sx={{py:"11px"}} variant='contained'>
+              {auth.loading? <CircularProgress size={24} color='success'/>:"send otp"}
+            </Button>
+          </div>
+          }
+          {auth.otp &&
+            <div className="space-y-2">
+              <p className="font-medium text-sm opacity-60">enter OTP sent to your email</p>
+              <TextField
+                  fullWidth
+                  name='otp'
+                  label="OTP"
+                  value={formik.values.otp}
+                  onChange={formik.handleChange}
+                  error={formik.touched.otp && Boolean(formik.errors.otp)}
+                  helperText={formik.touched.otp && formik.errors.otp}
+                  onBlur={formik.handleBlur}
+              />
+              <Button disabled={seller.loading} onClick={()=>formik.handleSubmit()} fullWidth sx={{py:"11px"}} variant='contained'>
+                {seller.loading? <CircularProgress size={24} color='success'/>:"login"}
+              </Button>
+          </div>
+          }
 
     </div>
     </Box>
